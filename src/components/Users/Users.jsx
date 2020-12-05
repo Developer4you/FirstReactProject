7971f8912ffe25d0dd7222b9c44d655f13ee47c1
Developer4you <avervalpet@gmail.com>
2020-12-05 22:18:03 +0300

    finished 55th lesson - userpage & ajax

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -57,22 +57,40 @@ import userPhoto from '../../avatar.jpg';
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
                 .then(response => {
                     this.props.setUsers(response.data.items);
-
+                    this.props.setTotalUsersCount(response.data.totalCount);
                 })
     }
 
-render()
-{
-    return <div>
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            })
+    }
 
+render() {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i=1; i <= pagesCount; i++){
+            pages.push(i);
+        }
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={this.props.currentPage === p && styles.selectedPage }
+                onClick = {(e) => {this.onPageChanged(p);}}>{p}</span>
+        })}
+
+        </div>
         {
             this.props.users.map(u => <div className={styles.userPost} key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                            <img alt="Avatar" src={u.photos.small != null ? u.photos.small : userPhoto}
                                  className={styles.userPhoto}/>
                         </div>
                         <div>
@@ -103,8 +121,8 @@ render()
                     </span>
             </div>)
         }
-    </div>
-}
+            </div>
+        }
 }
 
 export default Users;
